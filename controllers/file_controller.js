2165d759133f5a50d8f096f3eb9f7e9be35d3720
Dapo Paul Ogunlana <dapoogunlana@gmail.com>
2020-05-07 18:00:34 +0100

    files added

--- controllers/file_controller.js
+++ controllers/file_controller.js
@@ -6,14 +6,54 @@ const path = require('path')
 
 
 
-router.get('/xlsx', (req, res)=>{
-    res.sendFile(path.join(__dirname, '../files/report.xlsx'))
-})
-router.get('/pdf', (req, res)=>{
-    res.sendFile(path.join(__dirname, '../files/report.pdf'))
-})
-router.get('/csv', (req, res)=>{
-    res.sendFile(path.join(__dirname, '../files/report.csv'))
+// router.get('/xlsx', (req, res)=>{
+//     res.sendFile(path.join(__dirname, '../files/report.xlsx'))
+// })
+// router.get('/pdf', (req, res)=>{
+//     res.sendFile(path.join(__dirname, '../files/report.pdf'))
+// })
+// router.get('/csv', (req, res)=>{
+//     res.sendFile(path.join(__dirname, '../files/report.csv'))
+// })
+
+router.get('/:format/:state/:typeCode', (req, res)=>{
+    if(req.params.format === 'csv'){
+        if (req.params.state === 'all') {
+            if (req.params.typeCode == '1') {
+                res.sendFile(path.join(__dirname, '../files/general-inflow.csv'))
+            } else if (req.params.typeCode == '2') {
+                res.sendFile(path.join(__dirname, '../files/general-outflow.csv'))
+            } else if (req.params.typeCode == '3') {
+                res.sendFile(path.join(__dirname, '../files/general-report.csv'))
+            }
+        } else if (req.params.state === 'lagos') {
+            if (req.params.typeCode == '1') {
+                res.sendFile(path.join(__dirname, '../files/lagos-inflow.csv'))
+            } else if (req.params.typeCode == '2') {
+                res.sendFile(path.join(__dirname, '../files/lagos-outflow.csv'))
+            } else if (req.params.typeCode == '3') {
+                res.sendFile(path.join(__dirname, '../files/lagos-report.csv'))
+            }
+        }
+    } else if (req.params.format === 'pdf') {
+        if (req.params.state === 'all') {
+            if (req.params.typeCode == '1') {
+                res.sendFile(path.join(__dirname, '../files/general-inflow.pdf'))
+            } else if (req.params.typeCode == '2') {
+                res.sendFile(path.join(__dirname, '../files/general-outflow.pdf'))
+            } else if (req.params.typeCode == '3') {
+                res.sendFile(path.join(__dirname, '../files/general-report.pdf'))
+            }
+        } else if (req.params.state === 'lagos') {
+            if (req.params.typeCode == '1') {
+                res.sendFile(path.join(__dirname, '../files/lagos-inflow.pdf'))
+            } else if (req.params.typeCode == '2') {
+                res.sendFile(path.join(__dirname, '../files/lagos-outflow.pdf'))
+            } else if (req.params.typeCode == '3') {
+                res.sendFile(path.join(__dirname, '../files/lagos-report.pdf'))
+            }
+        }
+    }
 })
 
 module.exports = router
